1125b92e72d765150785751499da380e1503da15
No <devbotdev@gmail.com>
2024-10-01 11:49:58 +0200

    Added the first volume slider in the options menu including the css file, volume slider ranges from -80dB to 0dB

--- src/main/java/me/ib/PersonalProject/control/MainMenuController.java
+++ src/main/java/me/ib/PersonalProject/control/MainMenuController.java
@@ -3,24 +3,20 @@ package me.ib.PersonalProject.control;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import me.ib.PersonalProject.Main;
 import me.ib.PersonalProject.util.State;
 import me.ib.PersonalProject.util.Utility;
 
 import java.io.IOException;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicReference;
 
 public final class MainMenuController {
 
@@ -53,47 +49,15 @@ public final class MainMenuController {
 
             Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getClassLoader().getResource("me.ib/titleoptions.fxml")));
             Stage newStage = new Stage();
-
-            BorderPane titleBar = new BorderPane();
-            titleBar.setPrefHeight(30);
-            titleBar.setStyle("-fx-background-color: #333;");
-
-            Label titleLabel = new Label("Options");
-            titleLabel.setStyle("-fx-text-fill: #fff;");
-            titleLabel.setAlignment(Pos.CENTER);
-
-            titleBar.setCenter(titleLabel);
-
-            BorderPane newRoot = new BorderPane();
-            newRoot.setTop(titleBar);
-            newRoot.setCenter(root);
-
-            Scene scene = new Scene(newRoot);
-
-            Button closeButton = new Button("X");
-            closeButton.setOnAction(event2 -> newStage.close());
-            titleBar.setRight(closeButton);
-
-            AtomicReference<Double> xOffset = new AtomicReference<>((double) 0);
-            AtomicReference<Double> yOffset = new AtomicReference<>((double) 0);
-
-            titleBar.setOnMousePressed(event3 -> {
-                xOffset.set(event3.getSceneX());
-                yOffset.set(event3.getSceneY());
-            });
-
-            titleBar.setOnMouseDragged(event3 -> {
-                newStage.setX(event3.getScreenX() - xOffset.get());
-                newStage.setY(event3.getScreenY() - yOffset.get());
-            });
+            Scene scene = new Scene(root);
 
             newStage.initOwner(((Node) event.getSource()).getScene().getWindow());
             newStage.initModality(Modality.APPLICATION_MODAL);
-            newStage.initStyle(StageStyle.UNDECORATED);
-
             newStage.setScene(scene);
 
-            newStage.setOnCloseRequest(event1 -> Utility.state = State.MAIN_MENU);
+            newStage.setTitle("Options");
+
+            newStage.getIcons().add(Utility.getResourceAsImage(Utility.optionsIcon));
 
             newStage.show();
         } catch (IOException ioException) {
--- src/main/java/me/ib/PersonalProject/util/Utility.java
+++ src/main/java/me/ib/PersonalProject/util/Utility.java
@@ -1,5 +1,17 @@
 package me.ib.PersonalProject.util;
 
-public class Utility {
+import javafx.scene.image.Image;
+import me.ib.PersonalProject.Main;
+
+import java.io.InputStream;
+import java.util.Objects;
+
+public abstract class Utility {
     public static State state;
+    public static final String optionsIcon = "optionsIcon.png";
+
+    //Should be png icon
+    public static Image getResourceAsImage(String fileName) {
+        return new Image(Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("me.ib/" + fileName)));
+    }
 }
